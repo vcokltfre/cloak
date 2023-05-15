c1d8af639dd657bab10afef6f8b228b7ba1deeab
vcokltfre <vcokltfre@gmail.com>
2023-05-16 00:47:40 +0100

    feat: add initial route stubs

--- src/api/__main__.py
+++ src/api/__main__.py
@@ -5,10 +5,20 @@ load_dotenv()
 
 from src.database import connect
 
+from .channels import router as channels_router
+from .messages import router as messages_router
+from .nodes import router as nodes_router
+from .users import router as users_router
+
 app = FastAPI(
     title="Cloak",
     docs_url="/",
 )
+
+app.include_router(channels_router)
+app.include_router(messages_router)
+app.include_router(nodes_router)
+app.include_router(users_router)
 
 
 @app.on_event("startup")
---
+++ src/api/messages/routes.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/channels/{channel_id}/messages", tags=["Messages"])
